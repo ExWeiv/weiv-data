ecd16f567b73ed6619762e50c876d7c14277d11e
Enes Bekar <aydontloss@gmail.com>
2024-01-08 17:31:38 +0200

    Imported to Wix Blocks

--- app/src/Connection/secret_helpers.ts
+++ app/src/Connection/secret_helpers.ts
@@ -1,27 +1,27 @@
-// //@ts-ignore
-// import { secrets } from "wix-secrets-backend.v2"; //@ts-ignore
-// import * as wixAuth from 'wix-auth';
+//@ts-ignore
+import { secrets } from "wix-secrets-backend.v2"; //@ts-ignore
+import * as wixAuth from 'wix-auth';
 
-// const getSecretValue = wixAuth.elevate(secrets.getSecretValue);
+const getSecretValue = wixAuth.elevate(secrets.getSecretValue);
 
-// /**
-//  * @description Get's the secret data (connection URI) and saves it to global variable with cache feature.
-//  * @param secretName Secret's name
-//  * @returns The secret/URI for given secret name.
-//  */
-// export async function getCachedSecret(secretName: string): Promise<string> {
-//     try { //@ts-ignore
-//         let secret = global[secretName.toUpperCase()];
+/**
+ * @description Get's the secret data (connection URI) and saves it to global variable with cache feature.
+ * @param secretName Secret's name
+ * @returns The secret/URI for given secret name.
+ */
+export async function getCachedSecret(secretName: string): Promise<string> {
+    try { //@ts-ignore
+        let secret = global[secretName.toUpperCase()];
 
-//         if (!secret || secret === null || secret === undefined) {
-//             secret = await getSecretValue(secretName); //@ts-ignore
-//             global[secretName.toUpperCase()] = secret;
-//         }
+        if (!secret || secret === null || secret === undefined) {
+            secret = await getSecretValue(secretName); //@ts-ignore
+            global[secretName.toUpperCase()] = secret;
+        }
 
-//         return secret;
-//     } catch (err) {
-//         console.error(err);
-//         const visitorURI = await getSecretValue("visitorURI");
-//         return visitorURI;
-//     }
-// }
+        return secret;
+    } catch (err) {
+        console.error(err);
+        const visitorURI = await getSecretValue("visitorURI");
+        return visitorURI;
+    }
+}
