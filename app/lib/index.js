4983b63d57901c3d771d510d933f526beb83cedf
Enes <aydontloss@gmail.com>
2024-02-11 20:22:34 +0200

    import BUG fixed.

---
+++ app/lib/index.js
@@ -0,0 +1,41 @@
+"use strict";
+const data_aggregate_1 = require("./Aggregate/data_aggregate");
+const data_filter_1 = require("./Filter/data_filter");
+const data_query_1 = require("./Query/data_query");
+const queryReferenced_1 = require("./Functions/QueryReferenced/queryReferenced");
+const bulkInsert_1 = require("./Functions/bulkInsert");
+const bulkRemove_1 = require("./Functions/bulkRemove");
+const bulkSave_1 = require("./Functions/bulkSave");
+const bulkUpdate_1 = require("./Functions/bulkUpdate");
+const get_1 = require("./Functions/get");
+const insert_1 = require("./Functions/insert");
+const insertReference_1 = require("./Functions/insertReference");
+const isReferenced_1 = require("./Functions/isReferenced");
+const remove_1 = require("./Functions/remove");
+const removeReference_1 = require("./Functions/removeReference");
+const replaceReferences_1 = require("./Functions/replaceReferences");
+const save_1 = require("./Functions/save");
+const truncate_1 = require("./Functions/truncate");
+const update_1 = require("./Functions/update");
+const idConverter_1 = require("./Functions/idConverter");
+module.exports = {
+    aggregate: (collectionId) => new data_aggregate_1.WeivDataAggregate(collectionId),
+    filter: () => new data_filter_1.WeivDataFilter(),
+    query: (collectionId) => new data_query_1.WeivDataQuery(collectionId),
+    queryReferenced: queryReferenced_1.queryReferenced,
+    bulkInsert: bulkInsert_1.bulkInsert,
+    bulkRemove: bulkRemove_1.bulkRemove,
+    bulkSave: bulkSave_1.bulkSave,
+    bulkUpdate: bulkUpdate_1.bulkUpdate,
+    get: get_1.get,
+    insert: insert_1.insert,
+    insertReference: insertReference_1.insertReference,
+    isReferenced: isReferenced_1.isReferenced,
+    remove: remove_1.remove,
+    removeReference: removeReference_1.removeReference,
+    replaceReferences: replaceReferences_1.replaceReferences,
+    save: save_1.save,
+    truncate: truncate_1.truncate,
+    update: update_1.update,
+    idConvreter: idConverter_1.idConvreter,
+};
---
+++ app/src/index.ts
@@ -0,0 +1,44 @@
+import { WeivDataAggregate } from "./Aggregate/data_aggregate";
+import { WeivDataFilter } from "./Filter/data_filter";
+import { WeivDataQuery } from './Query/data_query';
+import { queryReferenced } from './Functions/QueryReferenced/queryReferenced';
+import { bulkInsert } from './Functions/bulkInsert';
+import { bulkRemove } from './Functions/bulkRemove';
+import { bulkSave } from './Functions/bulkSave';
+import { bulkUpdate } from './Functions/bulkUpdate';
+import { get } from './Functions/get';
+import { insert } from './Functions/insert';
+import { insertReference } from './Functions/insertReference';
+import { isReferenced } from './Functions/isReferenced';
+import { remove } from './Functions/remove';
+import { removeReference } from './Functions/removeReference';
+import { replaceReferences } from './Functions/replaceReferences';
+import { save } from './Functions/save';
+import { truncate } from './Functions/truncate';
+import { update } from './Functions/update';
+import { idConvreter } from './Functions/idConverter';
+
+/**@internal */
+import { CollectionID } from "./Helpers/collection";
+
+export = {
+    aggregate: (collectionId: CollectionID) => new WeivDataAggregate(collectionId),
+    filter: () => new WeivDataFilter(),
+    query: (collectionId: CollectionID) => new WeivDataQuery(collectionId),
+    queryReferenced,
+    bulkInsert,
+    bulkRemove,
+    bulkSave,
+    bulkUpdate,
+    get,
+    insert,
+    insertReference,
+    isReferenced,
+    remove,
+    removeReference,
+    replaceReferences,
+    save,
+    truncate,
+    update,
+    idConvreter,
+}
